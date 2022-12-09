ac2bcc6d3422e458bdf4468003bc5d59d2a5af83
Jan Staelens <jan.staelens@skyline.be>
2022-12-09 11:06:20 +0100

    Add project files.

---
+++ AcqDemo1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AcqDemo1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AcqDemo1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6BC49DBD-F9AD-475D-9DFB-20F3F55B4C1D")]
+[assembly: AssemblyVersion("1.0.0.0")]
